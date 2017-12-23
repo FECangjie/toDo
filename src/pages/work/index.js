7fa8a6901858be52b4018a1f10d25228f857bac2
Kai Kang <kakcool@qq.com>
2017-12-23 16:28:05 +0800

    add word part

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -7,38 +7,53 @@ export default Vue.component('WorkPage', {
   data () {
     return {
       pictureHouse:[
-        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/gg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/jz.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/kipor.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/qg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/smsg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tcx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tcx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/sszx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/sy.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/nioix.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/onlly.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/gg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/jz.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/kipor.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/qg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/smsg.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tcx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/tcx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/sszx.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/sy.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/nioix.jpg'},
-        {picturePath:'src/assets/img/pictureHouse/onlly.jpg'},
+        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg',title:'p1'},
+        {picturePath:'src/assets/img/pictureHouse/gg.jpg',title:'p2'},
+        {picturePath:'src/assets/img/pictureHouse/jz.jpg',title:'p13'},
+        {picturePath:'src/assets/img/pictureHouse/kipor.jpg',title:'p8'},
+        {picturePath:'src/assets/img/pictureHouse/qg.jpg',title:'p7'},
+        {picturePath:'src/assets/img/pictureHouse/smsg.jpg',title:'p6'},
+        {picturePath:'src/assets/img/pictureHouse/tcx.jpg',title:'p5'},
+        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg',title:'p4'},
+        {picturePath:'src/assets/img/pictureHouse/tcx.jpg',title:'p15'},
+        {picturePath:'src/assets/img/pictureHouse/sszx.jpg',title:'p18'},
+        {picturePath:'src/assets/img/pictureHouse/sy.jpg',title:'p17'},
+        {picturePath:'src/assets/img/pictureHouse/nioix.jpg',title:'p19'},
+        {picturePath:'src/assets/img/pictureHouse/onlly.jpg',title:'p10'},
+        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg',title:'p13'},
+        {picturePath:'src/assets/img/pictureHouse/gg.jpg',title:'p12'},
+        {picturePath:'src/assets/img/pictureHouse/jz.jpg',title:'p11'},
+        {picturePath:'src/assets/img/pictureHouse/kipor.jpg',title:'p31'},
+        {picturePath:'src/assets/img/pictureHouse/qg.jpg',title:'p11'},
+        {picturePath:'src/assets/img/pictureHouse/smsg.jpg',title:'p11'},
+        {picturePath:'src/assets/img/pictureHouse/tcx.jpg',title:'p123'},
+        {picturePath:'src/assets/img/pictureHouse/tuyue.jpg',title:'12p1'},
+        {picturePath:'src/assets/img/pictureHouse/tcx.jpg',title:'p14a'},
+        {picturePath:'src/assets/img/pictureHouse/sszx.jpg',title:'p1aw2'},
+        {picturePath:'src/assets/img/pictureHouse/sy.jpg',title:'p134'},
+        {picturePath:'src/assets/img/pictureHouse/nioix.jpg',title:'p1x3'},
+        {picturePath:'src/assets/img/pictureHouse/onlly.jpg',title:'p2'},
       ],
+      pictureHouse1:[],
+      pictureHouse2:[],
+      pictureHouse3:[],
+      pictureHouse4:[],
 
     }
   },
   methods: {
     func(){
+      for (let picture of this.pictureHouse) {
+        if (this.pictureHouse1.length > this.pictureHouse2.length ) {
+          this.pictureHouse2.push(picture);
+        } else if (this.pictureHouse2.length > this.pictureHouse3.length) {
+          this.pictureHouse3.push(picture);
+        } else if (this.pictureHouse3.length > this.pictureHouse4.length) {
+          this.pictureHouse4.push(picture);
+        } else {
+          this.pictureHouse1.push(picture);
+        }
+      }
       
     }
   },
