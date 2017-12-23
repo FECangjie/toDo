e62ab5f18482ae46fd0ec95890796f15ab9f2ad3
QinChao <qinchao@lianjia.com>
2017-12-23 17:17:34 +0800

    简历和接口

---
+++ mock/info.js
@@ -0,0 +1,21 @@
+module.exports = function (path, param, context) {
+  var result = {
+    "code": 1,
+    "msg": "成功",
+    "data": {
+      info: {
+  			name: '秦超',
+  			xueli: '本科985',
+  			tel: '131-2156-1943',
+  			email: 'qinchao7n@qq.com',
+        age: '25',
+        workYear: '2-3',
+        zhengzhi: '中共党员',
+        marry: '未婚',
+        address: '北京',
+        hometown: '吉林长春',
+  		}
+    }
+  }
+  return result
+}
--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import './style.less'
 import tpl from './tpl.vtpl'
+import store from 'store'
 
 export default Vue.component('resume', {
   data () {
     return {
-      isLoading: true,
+      info: {},
       imgUrl: 'src/assets/img/vue.png',
+    }
+  },
+  computed: {
+    getInfo () {
+      return this.$store.getters.getMeInfo
     }
   },
   created () {
     let me = this
+    store.dispatch('set_Info', {})
   },
   components: {
   },
